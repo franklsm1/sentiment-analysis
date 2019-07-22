a3a5b9029f0336cabacf6a8c70c0843bca061b7e
Sean Franklin <pair+franklsm1@gmail.com>
2019-07-22 17:25:01 -0400

    Fix npm audit issues and add husky pre-commit hook
    - add husky pre-commit hook to ensure tests are run prior to committing
    - upgrade to babel 7
    - changed db user and password env vars to be more specific

--- knexfile.js
+++ knexfile.js
@@ -2,8 +2,8 @@ module.exports = {
   development: {
     client: 'mysql',
     connection: {
-      user: process.env.dbUser || 'root',
-      password: process.env.dbPassword || '',
+      user: process.env.sentimentDbUser || 'root',
+      password: process.env.sentimentDbPassword || '',
       database: 'sentiment_db',
       timezone: 'UTC',
       charset: 'utf8mb4'
@@ -12,8 +12,8 @@ module.exports = {
   test: {
     client: 'mysql',
     connection: {
-      user: process.env.dbUser || 'root',
-      password: process.env.dbPassword || '',
+      user: process.env.sentimentDbUser || 'root',
+      password: process.env.sentimentDbPassword || '',
       database: 'sentiment_db_test',
       timezone: 'UTC',
       charset: 'utf8mb4'
