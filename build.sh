2440f5852e54dcc9f157b7052f95961d4623c20c
Sean Franklin <pair+franklsm1@gmail.com>
2019-08-14 07:18:50 -0400

    Update build script to use dot notation for dir

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ yarn clean
 yarn install
 yarn build
 cp package.json ./dist
-cp -r client/build ./dist/client
+cp -r ./client/build ./dist/client
 cd dist
 yarn install --prod
 # zip -r sentiment-analysis .
