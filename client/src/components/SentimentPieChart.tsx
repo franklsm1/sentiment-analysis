321c36e8e31134f43e694370b38b7c3bb126524a
Sean Franklin <pair+franklsm1@gmail.com>
2019-10-24 22:25:36 -0400

    Update husky version

--- client/src/components/SentimentPieChart.tsx
+++ client/src/components/SentimentPieChart.tsx
@@ -7,15 +7,15 @@ const COLORS = ['red', 'orange', 'green'];
 const RADIAN = Math.PI / 180;
 // @ts-ignore
 const renderLabel: React.ReactElement<any> = ({cx, cy, midAngle, innerRadius, outerRadius, percent}) => {
-      const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-      const x = cx + radius * Math.cos(-midAngle * RADIAN);
-      const y = cy + radius * Math.sin(-midAngle * RADIAN);
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
 
-      return percent > 0 ? (
-        <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
-          {`${(percent * 100).toFixed(0)}%`}
-        </text>
-      ) : '';
+  return percent > 0 ? (
+    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  ) : '';
 };
 
 const SentimentPieChart = ({ posts = {negative: [], neutral: [], positive: []} }: {posts?: Posts}) => {
@@ -24,7 +24,7 @@ const SentimentPieChart = ({ posts = {negative: [], neutral: [], positive: []} }
     { name: 'Neutral', value: posts.neutral.length },
     { name: 'Positive', value: posts.positive.length }
   ];
-    return (
+  return (
     <ResponsiveContainer aspect={4.0 / 3.0} width='100%'>
       <PieChart >
         <Legend verticalAlign="bottom" height={36}/>
